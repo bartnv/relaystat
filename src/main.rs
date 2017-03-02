cc581e7600a89a737de8a5cbf95c15409b2d950f
Bart Noordervliet <bart@noordervliet.net>
2017-03-02 13:05:44 +0100

    BufReader version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::process::exit;
 use std::net::{TcpStream, TcpListener};
-use std::thread::spawn;
-use std::io::Read;
+use std::thread;
+use std::io::BufRead;
+use std::io::BufReader;
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
@@ -9,31 +10,25 @@ fn main() {
     println!("Not enough arguments");
     exit(-1);
   }
-//  let src = args[1].parse().unwrap_or_else(|e| { println!("Invalid arguments"); exit(1); });
-//  let dst = args[2].parse().unwrap_or_else(|e| { println!("Invalid arguments"); exit(1); });
 
-  let (input, output) = std::sync::mpsc::channel::<Vec<u8>>();
+  let (input, output) = std::sync::mpsc::channel();
 
-  let server = TcpListener::bind(&*args[1]).unwrap_or_else(|_e| { println!("Failed to bind to listen address {}", args[1]); exit(1); });
+  let server = TcpListener::bind(&*args[1]).unwrap_or_else(|e| { println!("Failed to bind to listen address {}: {}", args[1], e.to_string()); exit(1); });
 //  let client = TcpStream::connect(&*args[2]).unwrap_or_else(|e| { println!("Failed to connect to address {}", args[2]); exit(1); });
 
-  spawn(move || {
+  thread::spawn(move || {
     loop {
       match server.accept() { // Might also iterate over incoming() here
         Ok(res) => {
-          let (mut stream, addr) = res;
+          let (stream, addr) = res;
           let my_input = input.clone();
           println!("New incoming connection from {}", addr);
-          spawn(move || {
-            loop {
-              let mut buf = vec![0; 5];
-              let n = stream.read(&mut buf).unwrap();
-              if n == 0 {
-                println!("Connection from {} closed", addr);
-                return;
-              }
-              my_input.send(buf).unwrap();
+          thread::spawn(move || {
+            let stream = BufReader::new(stream);
+            for line in stream.lines() {
+              my_input.send(line.unwrap()).unwrap();
             }
+            println!("Connection from {} closed", addr);
           });
         }
         Err(_) => panic!("Error in accept()")
@@ -42,7 +37,7 @@ fn main() {
   });
 
   loop {
-    println!("Input: {:?}", String::from_utf8(output.recv().unwrap()).unwrap());
+    println!("Input: {}", output.recv().unwrap());
+    // send to client
   }
-  // send to client
 }
