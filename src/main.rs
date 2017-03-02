bec904c664f064868a4dde4cc27c6478691119f9
Bart Noordervliet <bart@noordervliet.net>
2017-03-02 22:41:47 +0100

    Basic functionality complete

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::{TcpStream, TcpListener};
 use std::thread;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::io::Write;
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
@@ -11,11 +12,9 @@ fn main() {
     exit(-1);
   }
 
-  let (input, output) = std::sync::mpsc::channel();
-
   let server = TcpListener::bind(&*args[1]).unwrap_or_else(|e| { println!("Failed to bind to listen address {}: {}", args[1], e.to_string()); exit(1); });
-//  let client = TcpStream::connect(&*args[2]).unwrap_or_else(|e| { println!("Failed to connect to address {}", args[2]); exit(1); });
 
+  let (input, output) = std::sync::mpsc::channel();
   thread::spawn(move || {
     for res in server.incoming() {
       let stream = res.unwrap();
@@ -32,8 +31,27 @@ fn main() {
     }
   });
 
+  let mut client = TcpStream::connect(&*args[2]).unwrap_or_else(|e| { println!("Failed to connect to address {}: {}", args[2], e.to_string()); exit(1); });
+  println!("Outgoing connection to {} established", args[2]);
+  let mut lines_written = 0;
+  let recon_delay = std::time::Duration::new(60, 0);
+
   loop {
-    println!("Input: {}", output.recv().unwrap());
-    // send to client
+    let mut line = output.recv().unwrap();
+    line.push('\n');
+    match client.write_all(line.as_bytes()) {
+      Ok(_) => {
+        lines_written += 1;
+        if lines_written%100 == 0 { println!("{} lines received", lines_written); }
+      }
+      Err(e) => {
+        println!("Write error to {} ({}); reconnecting...", client.peer_addr().unwrap(), e.to_string());
+        thread::sleep(recon_delay);
+        match TcpStream::connect(&*args[2]) {
+          Ok(stream) => { client = stream; }
+          Err(e) => { println!("Failed to connect to address {}: {}", args[2], e.to_string()); }
+        }
+      }
+    }
   }
 }
