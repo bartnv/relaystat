7f78634ab6a15f2d9a33f4534e36d4bcbe5d7fda
Bart Noordervliet <bart@noordervliet.net>
2017-03-04 23:31:36 +0100

    Create a separate connect() function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,26 @@ use std::thread;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Write;
+use std::time::Duration;
+use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+
+static GLOBAL_THREAD_COUNT: AtomicUsize = ATOMIC_USIZE_INIT;
+
+fn connect(addr: &str) -> TcpStream {
+  let recon_delay = Duration::new(30, 0);
+  loop {
+    match TcpStream::connect(addr) {
+      Ok(stream) => {
+        println!("Outgoing connection to {} established", addr);
+        return stream;
+      }
+      Err(e) => println!("Failed to connect to {}: {}", addr, e.to_string())
+    }
+    recon_delay + Duration::new(30, 0);
+    println!("Delaying reconnect for {:?}", recon_delay);
+    thread::sleep(recon_delay);
+  }
+}
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
@@ -22,34 +42,33 @@ fn main() {
       let my_input = input.clone();
       println!("New incoming connection from {}", addr);
       thread::spawn(move || {
+        GLOBAL_THREAD_COUNT.fetch_add(1, Ordering::SeqCst);
         let stream = BufReader::new(stream);
         for line in stream.lines() {
           my_input.send(line.unwrap()).unwrap();
         }
         println!("Connection from {} closed", addr);
+        GLOBAL_THREAD_COUNT.fetch_sub(1, Ordering::SeqCst);
       });
     }
   });
 
-  let mut client = TcpStream::connect(&*args[2]).unwrap_or_else(|e| { println!("Failed to connect to address {}: {}", args[2], e.to_string()); exit(1); });
-  println!("Outgoing connection to {} established", args[2]);
+  let mut client = connect(&*args[2]);
   let mut lines_written = 0;
-  let recon_delay = std::time::Duration::new(60, 0);
 
   loop {
     let mut line = output.recv().unwrap();
     line.push('\n');
-    match client.write_all(line.as_bytes()) {
-      Ok(_) => {
-        lines_written += 1;
-        if lines_written%100 == 0 { println!("{} lines received", lines_written); }
-      }
-      Err(e) => {
-        println!("Write error to {} ({}); reconnecting...", client.peer_addr().unwrap(), e.to_string());
-        thread::sleep(recon_delay);
-        match TcpStream::connect(&*args[2]) {
-          Ok(stream) => { client = stream; }
-          Err(e) => { println!("Failed to connect to address {}: {}", args[2], e.to_string()); }
+    loop {
+      match client.write_all(line.as_bytes()) {
+        Ok(_) => {
+          lines_written += 1;
+          if lines_written%100 == 0 { println!("{:?} incoming connections | {} lines received", GLOBAL_THREAD_COUNT, lines_written); }
+          break;
+        }
+        Err(e) => {
+          println!("Write error [{}]; reconnecting...", e.to_string());
+          client = connect(&*args[2]);
         }
       }
     }
